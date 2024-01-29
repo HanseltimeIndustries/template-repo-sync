eb717dfb19b3b17b2e187e1c47cac1e20115e84b
hanseltime <hanseltime@gmail.com>
2024-01-28 23:45:25 -0700

    test: wip

--- src/huh.ts
+++ src/huh.ts
@@ -2,6 +2,8 @@ import { readFileSync } from "fs";
 import { syncGithubRepo } from "./ci-actions/github";
 import { join } from "path";
 
+process.env.GITHUB_REPOSITORY = 'HanseltimeIndustries/test-private-template'
+
 void syncGithubRepo({
     githubToken: readFileSync(join('tmp', 'token')).toString(),
     repoPath: 'HanseltimeIndustries/test-private-template',
