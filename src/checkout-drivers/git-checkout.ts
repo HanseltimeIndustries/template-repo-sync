ad7ba7dc795883f813f70ed2e9140ac6a9030845
hanseltime <hanseltime@gmail.com>
2024-06-09 15:50:26 -0600

    fix: skipping checkout default is specified

--- src/checkout-drivers/git-checkout.ts
+++ src/checkout-drivers/git-checkout.ts
@@ -9,14 +9,30 @@ export async function gitCheckout(options: {
   branch: string;
 }): Promise<boolean> {
   const { branch, remoteName, tmpDir } = options;
-  execSync(`git fetch ${remoteName} ${branch}`, {
+
+  const remoteInfo = execSync(`git remote show ${remoteName}`, {
     cwd: tmpDir,
     env: process.env,
-  });
-  execSync(`git checkout -b ${branch} --track ${remoteName}/${branch}`, {
-    cwd: tmpDir,
-    env: process.env,
-  });
+  }).toString();
+  const defaultMatch = /HEAD branch:\s*([a-zA-Z0-9_-]+)/.exec(remoteInfo);
+  if (!defaultMatch || !defaultMatch[1]) {
+    throw new Error(
+      `Could not determine default branch of cloned repo.\nAttempted to find in remote info:\n${remoteInfo} `,
+    );
+  }
+
+  const defaultBranch = defaultMatch[1];
+  // Skip this if the default branch is already pulled
+  if (defaultBranch !== branch) {
+    execSync(`git fetch ${remoteName} ${branch}`, {
+      cwd: tmpDir,
+      env: process.env,
+    });
+    execSync(`git checkout -b ${branch} --track ${remoteName}/${branch}`, {
+      cwd: tmpDir,
+      env: process.env,
+    });
+  }
 
   return true;
 }
